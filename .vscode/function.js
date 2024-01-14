ccf5175a5872e982d1d5b85ff572d0798ac0caad
Funkazan <128518893+Funkazan@users.noreply.github.com>
2024-01-14 15:56:30 +0100

    Update

---
+++ .vscode/function.js
@@ -0,0 +1,4 @@
+// JavaScript to toggle the visibility of the menu list on button click
+document.querySelector('.menu-button').addEventListener('click', function() {
+    document.querySelector('.menu-list').classList.toggle('show-menu');
+});
