36ef386d61825ce5b4216623d3a7ef861593d9f3
Funkazan <128518893+Funkazan@users.noreply.github.com>
2024-01-21 09:10:36 +0100

    update

--- .vscode/function.js
+++ .vscode/function.js
@@ -1,38 +1,41 @@
-// JavaScript to toggle the visibility of the menu list based on screen width
 document.addEventListener('DOMContentLoaded', function () {
     var mobileMenu = document.querySelector('.mobile-menu');
     var menuButton = document.querySelector('.menu-button');
     var menuList = mobileMenu.querySelector('.menu-list');
 
-    //console.log('mobileMenu:', mobileMenu);
-    //console.log('menuButton:', menuButton);
+    function showMenu() {
+        mobileMenu.classList.add('active');
+        menuList.style.display = 'flex';
+        menuList.style.flexDirection = 'column'; // Set flex-direction to column
+    }
+
+    function hideMenu() {
+        mobileMenu.classList.remove('active');
+        menuList.style.display = 'none';
+    }
 
     function toggleMenu() {
         mobileMenu.classList.toggle('active');
 
-        // Toggle the menu-list based on the active class
+        // Toggle the menu-list basen on the active class
+        menuList.classList.toggle('show');
         if (mobileMenu.classList.contains('active')) {
-            menuList.style.display = 'flex';
-        } else {
-            // Add a class to handle the delay before hiding the menu
-            menuList.classList.add('leave');
-            setTimeout(() => {
-                menuList.style.display = 'none';
-                // Remove the class after the delay
-                menuList.classList.remove('leave');
-            }, 1000); //Adjust the delay time (in milliseconds) as needed
+            menuList.style.flexDirection = 'column'; // Set flex-direction to column when menu is active
         }
     }
 
-    //Hide menu list by default
-    menuList.style.display = 'none';
-
     // Enable click functionality for mobile
-    menuButton.addEventListener('click', toggleMenu);
+    menuButton.addEventListener('click', function () {
+        if (mobileMenu.classList.contains('active')) {
+            hideMenu();
+        } else {
+            showMenu();
+        }
+    });
 
     // Enable hover functionality for desktop
     if (window.innerWidth > 768) {
-        menuButton.addEventListener('mouseenter', toggleMenu);
-        menuButton.addEventListener('mouseleave', toggleMenu);
+        menuButton.addEventListener('mouseenter', showMenu);
+        mobileMenu.addEventListener('mouseleave', hideMenu);
     }
 });
